903bbf84d1ef984e60b7d6376b1965407a0b7c42
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2017-05-23 17:03:28 +0900

    Initial Commit

---
+++ CocoaPodsIntro/CocoaPodsIntro.h
@@ -0,0 +1,19 @@
+//
+//  CocoaPodsIntro.h
+//  CocoaPodsIntro
+//
+//  Created by 有村 皓太郎 on 2017/05/23.
+//  Copyright © 2017年 hormiga6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaPodsIntro.
+FOUNDATION_EXPORT double CocoaPodsIntroVersionNumber;
+
+//! Project version string for CocoaPodsIntro.
+FOUNDATION_EXPORT const unsigned char CocoaPodsIntroVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaPodsIntro/PublicHeader.h>
+
+
